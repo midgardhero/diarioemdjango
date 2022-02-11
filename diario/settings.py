39895dbe745d8ec02508d379e2e61783fb8f84b5
Leandro Fischer <fischer476@gmail.com>
2022-02-11 20:03:38 -0300

    Add conection app entries to project diário

--- diario/settings.py
+++ diario/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "entries.apps.EntriesConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
