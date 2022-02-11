1f5092c9ae5edf69004e0d41de15e77d558d6b7c
Leandro Fischer <fischer476@gmail.com>
2022-02-11 20:49:16 -0300

    create configs lists views

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,16 @@
+from pyexpat import model
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
 
+from .models import Entry
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-data_created")
+
+class EntryDetailView(DetailView):
+    model = Entry
 # Create your views here.
