6720f071eb7e6413d2c697265c98a84695ce23a6
Leandro Fischer <fischer476@gmail.com>
2022-02-14 18:22:40 -0300

    Update views

--- entries/urls.py
+++ entries/urls.py
@@ -1,25 +1,13 @@
-
-
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    path("", views.EntryListView.as_view(), name ="entry-list"),
     path(
-        '',
-        views.EntryListView.as_view(),
-        name ='entry-list'
+        "entry/<int:pk>", views.EntryDetailView.as_view(), name='entry-detail'
     ),
-    path(
-        'entry/<int:pk>',
-        views.EntryDetailView.as_view(),
-        name='entry-detail'
-    ),
-    path(
-        "create",
-        views.EntryCreateView.as_view(),
-        name="entry-create"
-    ),
+    path("create", views.EntryCreateView.as_view(), name="entry-create"),
     path(
         "entry/<int:pk>/update",
         views.EntryUpdateView.as_view(),
--- entries/views.py
+++ entries/views.py
@@ -1,7 +1,5 @@
-from pyexpat import model
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -13,28 +11,31 @@ from django.views.generic import (
 
 from .models import Entry
 
+
 class EntryListView(ListView):
     model = Entry
-    queryset = Entry.objects.all().order_by("date_created")
+    queryset = Entry.objects.all().order_by("-date_created")
+
 
 class EntryDetailView(DetailView):
     model = Entry
+
 
 class EntryCreateView(SuccessMessageMixin, CreateView):
     model = Entry
     fields = ['title', 'content']
     sucess_url = reverse_lazy('entry-list')
     success_message = "mensagem criada!"
+
+
 class EntryUpdateView(SuccessMessageMixin, UpdateView):
     model = Entry
     fields = ["title", "content"]
     success_message = "mensagem salva!"
 
     def get_success_url(self):
-        return reverse_lazy(
-            "entry-detail",
-            kwargs={"pk": self.entry.id}
-        )
+        return reverse_lazy("entry-detail", kwargs={"pk": self.object.pk})
+
 
 class EntryDeleteView(DeleteView):
     model = Entry
@@ -44,5 +45,3 @@ class EntryDeleteView(DeleteView):
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
         return super().delete(request, *args, **kwargs)
-
-# Create your views here.
