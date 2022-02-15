9941dcc7ef828f03599fdd2d016a7a55b11873d8
Leandro Fischer <fischer476@gmail.com>
2022-02-15 19:58:32 -0300

    import render

--- entries/views.py
+++ entries/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
