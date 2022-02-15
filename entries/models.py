b98144f029799e74be0b117b0ec3391e18c8f03e
Leandro Fischer <fischer476@gmail.com>
2022-02-14 21:23:44 -0300

    att models, add absolute URL and config timezone

--- entries/models.py
+++ entries/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.urls import reverse
 
 class Entry(models.Model):
     title = models.CharField(max_length=200)
@@ -10,5 +10,7 @@ class Entry(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('entry-list')
     class Meta:
         verbose_name_plural = "Entries"
